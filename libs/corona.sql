777e5a5e95b62e1016d2cf7d00cdd42c8ea23170
Ondrej Flidr <flidr@vshosting.cz>
2018-01-12 18:12:01 +0100

    SQL dump moved

--- libs/corona.sql
+++
@@ -1,8 +0,0 @@
---- List of known agents
-create table agents(
-    agent_id int not null primary key auto_increment,
-    agent_ip int not null,
-    agent_port int not null default 36788,
-    agent_version float not null,
-    is_online tinyint default 0
-);
